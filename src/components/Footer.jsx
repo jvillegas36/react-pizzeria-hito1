7df94cf4308869d11169a71b02b3a9d28159d1ba
choche36 <jvillegasn@gmail.com>
2025-06-01 12:45:20 -0400

    eitar footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <footer class=" col-12 py-4 text-center text-white ">
       <div class="row justify-content-center">
-        © 2021 - Pizzería Mamma Mia! - Todos los derechos reservados
+        © 2021 - Pizzería Mamma Mia! - Todos los derechos reservados.
       </div>
     </footer>
   )

5fb2e5a61d17eafb3b5adde6c156cc1d89882df4
choche36 <jvillegasn@gmail.com>
2025-06-13 14:10:28 -0400

    Hito 3  cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Cart = () => {
   return (
-    <div>Cart</div>
+      <div>
+          
+
+
+          
+            <ul class="list-group">
+            <li class="list-group-item">An item</li>
+            <li class="list-group-item">A second item</li>
+            <li class="list-group-item">A third item</li>
+            <li class="list-group-item">A fourth item</li>
+            <li class="list-group-item">And a fifth one</li>
+            </ul>
+
+    </div>
   )
 }
 

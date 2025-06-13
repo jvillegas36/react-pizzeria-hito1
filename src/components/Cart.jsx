d743b912ab03c575358e7ed4d2f86fa4e1c72d73
choche36 <jvillegasn@gmail.com>
2025-06-13 14:13:44 -0400

    Hito 3  cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,23 +1,73 @@
-import React from 'react'
+import React, { useState } from "react";
+import { pizzaCart } from "../assets/pizzas";
 
 const Cart = () => {
+  const [carro, setCarro] = useState(pizzaCart);
+  const editPizza = [...carro];
+
+  const aumentarCompra = (piz) => {
+    const index = editPizza.findIndex((el) => el.id === piz.id);
+
+    carro[index].price = (piz.price / piz.count) * (piz.count + 1);
+    carro[index].count = piz.count + 1;
+    setCarro([...carro]);
+  };
+
+  const disminuirCompra = (piz) => {
+    const editPizza = [...carro];
+    const index = editPizza.findIndex((el) => el.id === piz.id);
+    if (editPizza[index].count - 1 == 0) {
+      carro.splice(index, 1);
+        setCarro([...carro]);
+        return
+    } else {
+      carro[index].price = (piz.price / piz.count) * (piz.count - 1);
+      carro[index].count = piz.count - 1;
+        setCarro([...carro]);
+        return
+    }
+    };
+    
+    const result = carro.reduce((total, currentValue) => total = total + currentValue.price,0);
+
   return (
-      <div>
-          
+    <div className="my-3 card container ">
+      <div className="d-flex justify-content-center">
+        <div className="row col-6 ">
+          <h3 className="text-start fw-bold">Detalle del pedido</h3>
 
+          <ul className="ulFormat">
+            {carro.map((piz) => (
+              <li key={piz.id} className="my-3">
+                <img className="imgLi col-2" src={piz.img} alt={piz.name} />
+                <label className="col-3 text-start ms-2 text-capitalize fw-semibold">
+                  {piz.name}
+                </label>
+                <label className="col-3 fw-semibold">$ {Intl.NumberFormat().format(piz.price)}</label>
 
-          
-            <ul class="list-group">
-            <li class="list-group-item">An item</li>
-            <li class="list-group-item">A second item</li>
-            <li class="list-group-item">A third item</li>
-            <li class="list-group-item">A fourth item</li>
-            <li class="list-group-item">And a fifth one</li>
-            </ul>
+                <button
+                  className="btn btn-outline-danger btnAncho"
+                  onClick={() => disminuirCompra(piz)}
+                >
+                  -
+                </button>
+                <label className="col-1 fw-bold">{piz.count}</label>
+                <button
+                  className="btn btn-outline-primary btnAncho"
+                  onClick={() => aumentarCompra(piz)}
+                >
+                  +
+                </button>
+              </li>
+            ))}
+          </ul>
 
+          <h2 className="text-start fw-bold">Total : $  {Intl.NumberFormat().format(result)}</h2>
+          <button className="my-5 col-3 btn btn-primary fw-bold">Pagar</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
-
+export default Cart;

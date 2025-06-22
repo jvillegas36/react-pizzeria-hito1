3027dd9af582cb497108cb7516733f56a1c2a01e
choche36 <jvillegasn@gmail.com>
2025-06-22 15:58:46 -0400

    Hito 4  v2

--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -15,29 +15,33 @@ const Pizza = () => {
   };
 
   return (
-    <div className="card mx-2 my-5 ">
-      <div className="d-flex justify-content-center">
-        <div className="col-6">
-          <div className="row  justify-content-center">
-            <img src={pizza.img} className="imgPizza" />
-          </div>
-          <div className="row text-capitalize fw-semibold mt-3">
-            Pizza: {pizza.name}
-          </div>
+    <div className="d-flex justify-content-center">
+      <div className="col-8 card mx-2 my-5 text-dark shadow">
+        <div className="d-flex justify-content-center">
+          <div className="col-8">
+            <div className="row  justify-content-center">
+              <img src={pizza.img} className="imgPizza" />
+            </div>
+            <div className="row text-capitalize fw-semibold mt-3">
+              Pizza: {pizza.name}
+            </div>
 
-          <div className="row mt-3">
-            Precio: $ {Intl.NumberFormat().format(pizza.price)}
-                  </div>
-                  
-                  <div className="row mt-3 text-capitalize">
-                      Ingredientes: {pizza.ingredients.join(", ")}
-                  </div>
-                  <div className="row mt-3 text-start fs-6 fw-lighter">{pizza.desc}</div>
-                  
-                   <div class="mt-5">
-            <button type="button" class="btn btn-outline-dark">
-              Añadir a Carrito 🛒
-            </button>
+            <div className="row mt-3">
+              Precio: $ {Intl.NumberFormat().format(pizza.price)}
+            </div>
+
+            <div className="row mt-3 text-capitalize">
+              Ingredientes: {pizza.ingredients?.join(", ")}
+            </div>
+            <div className="row mt-3 text-start fs-6 fw-lighter" >
+              {pizza.desc}
+            </div>
+
+            <div className="mt-5">
+              <button type="button" class="btn btn-success btn-outline-info">
+                Añadir a Carrito 🛒
+              </button>
+            </div>
           </div>
         </div>
       </div>
